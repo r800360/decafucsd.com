96c2a3f0f797d6d31ae66d2b0796ab239b71e0d1
SV <vshree852@gmail.com>
2024-08-27 22:58:16 -0700

    complete faq page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Sponsor from './pages/Sponsor/Sponsor';
+import FAQ from './pages/FAQ/FAQ';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Home/>
       <About/>
       <Sponsor/>
+      <FAQ/>
     </>
   );
 }
---
+++ src/pages/FAQ/FAQ.jsx
@@ -0,0 +1,16 @@
+import './FAQ.css'
+import Border from '../../assets/Border.svg'
+function FAQ() {
+    return(
+        <div className='faq'>
+            <img src={Border} alt=''></img>
+            <div className='title'><p>Frequently Asked Questions</p></div>
+            <div className='content'>
+                
+            </div>
+            <img src={Border} alt=''></img>
+        </div>
+    )
+}
+
+export default FAQ
