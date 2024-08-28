017c9dd242744bef58aca6179f014dbd20d90de4
SV <vshree852@gmail.com>
2024-08-28 14:41:23 -0700

    complete footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Sponsor from './pages/Sponsor/Sponsor';
 import FAQ from './pages/FAQ/FAQ';
+import Footer from './pages/Footer/Footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <About/>
       <Sponsor/>
       <FAQ/>
+      <Footer/>
     </>
   );
 }
