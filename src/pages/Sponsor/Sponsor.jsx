1f82a59c480f26864801202b9cfecda9cb04ca9e
SV <vshree852@gmail.com>
2024-09-17 22:36:31 -0700

    update logos and layout

--- src/pages/Sponsor/Sponsor.jsx
+++ src/pages/Sponsor/Sponsor.jsx
@@ -1,51 +1,33 @@
 import './Sponsor.css'
 import CoffeeMug from "../../assets/CoffeeMug.svg";
+import Dynovas from "../../logos/Dynovas Matrix Logo (Final).jpg"
+import LLNL from "../../logos/LLNLlogoBLACK.jpg"
+import Viasat from "../../logos/Viasat_2018_Gradient_Web (1).jpg"
 
 function Sponsor() {
     return(
         <div className='sponsor' id='sponsor'>
             <div className='title'><p>Sponsors</p></div>
             <div className='content'>
-                <p>Platinum</p>
-                <div className='plat'>
-                    <div>
-                        <img src={CoffeeMug} alt="coffee_mug" id='mug'></img>
-                        Future Sponsor
-                    </div>
-                </div>
                 <p>Gold</p>
                 <div className='gold'>
                     <div>
                         <img src={CoffeeMug} alt="coffee_mug" id='mug'></img>
-                        Future Sponsor
-                    </div>
-                    <div>
                         Coming Soon
-                    </div>
-                    <div>
-                        <img src={CoffeeMug} alt="coffee_mug" id='mug'></img>
-                        Future Sponsor
                     </div>
                 </div>
                 <p>Silver</p>
                 <div className='silver'>
+                    <img src={Dynovas} alt="dynovas" className='logo'></img>
+                    <img src={Viasat} alt="viasat" className='logo'></img>
+                    <img src={LLNL} alt="llnl" className='logo'></img>
                     <div>
                         <img src={CoffeeMug} alt="coffee_mug" id='mug'></img>
-                        Future Sponsor
-                    </div>
-                    <div>
                         Coming Soon
                     </div>
                     <div>
+                        <img src={CoffeeMug} alt="coffee_mug" id='mug'></img>
                         Coming Soon
-                    </div>
-                    <div>
-                        <img src={CoffeeMug} alt="coffee_mug" id='mug'></img>
-                        Future Sponsor
-                    </div>
-                    <div>
-                        <img src={CoffeeMug} alt="coffee_mug" id='mug'></img>
-                        Future Sponsor
                     </div>
                 </div>
             </div>
